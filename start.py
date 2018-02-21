bf47d272b6ce4a5e82f497e5ec52cf963d0fc180
Marat Gaptullin <tdk1243@mail.ru>
2018-02-21 13:43:26 +0300

    iii

---
+++ start.py
@@ -0,0 +1,8 @@
+from pytg import Telegram
+tg = Telegram(
+	telegram="/root/tg/bin/telegram-cli",
+	pubkey_file="/root/tg/tg-server.pub")
+receiver = tg.receiver
+sender = tg.sender
+
+sender.send_msg("Дима1", "Hello World!")
